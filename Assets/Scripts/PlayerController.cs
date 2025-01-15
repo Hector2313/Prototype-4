c325275959cb625ef1121f2b00f6b1a5d122fba4
Hector2313 <158297297+Hector2313@users.noreply.github.com>
2025-01-15 12:37:05 -0500

    4.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
    private Rigidbody playerRb;
    public float speed = 5.0f;
